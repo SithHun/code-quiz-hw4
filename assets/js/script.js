e0a796e3b45fbbe8c6211e17dccc90e78bc0889b
Seth Sun <sithsun@gmail.com>
2023-03-17 15:05:43 -0400

    changes index.html, css, js

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+var startEl = document.querySelector('#start')
+var container1El = document.querySelector('#container1')
+
+function hideAYRstart (event) {
+    container1El.removeChild
+    
+}
+    
+
+startEl.addEventListener('click', hideAYRstart )
